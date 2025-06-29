1efdb4137502b43c9631d2a80ebdd33ba14594ce
SamuelmdLow <samuel.m.d.low@gmail.com>
2025-06-29 23:27:38 +0000

    Move everything into single directory and change database to postgres

--- thoth.ubyssey.ca/thoth/settings.py
+++ thoth.ubyssey.ca/thoth/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = "thoth.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'thoth',
+        'USER': 'root',
+        'PASSWORD': 'secret',
+        'HOST': 'db',
+        'PORT': '5432',
+}}
 
 
 # Password validation
